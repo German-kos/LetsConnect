9e6e58c50fab80da4f0faff56cf942c9c5f7a8d9
GermanKos <kostiakovg@gmail.com>
2024-01-15 11:31:24 +0200

    React walking skeleton

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
 
 function App() {
-  return <h1>LetsConnect</h1>;
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/activities")
+      .then((response) => setActivities(response.data));
+  }, []);
+
+  return (
+    <div>
+      <Header as="h2" icon="users" content="LetsConnect" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
 }
 
 export default App;
